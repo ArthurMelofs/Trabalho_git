9fc64f4b91057f08d7eb29f8a07a984e29b4f32f
Arthur Melo <meloarthurfs@gmail.com>
2026-05-30 17:23:43 -0300

    Adição de task de update

--- src/app.py
+++ src/app.py
@@ -8,9 +8,11 @@ tasks = []
 def home():
     return "Sistema de Gerenciamento de Tarefas"
 
+
 @app.route("/tasks", methods=["GET"])
 def get_tasks():
     return jsonify(tasks)
+
 
 @app.route("/tasks", methods=["POST"])
 def create_task():
@@ -18,13 +20,28 @@ def create_task():
     tasks.append(task)
     return jsonify(task), 201
 
+
+@app.route("/tasks/<int:index>", methods=["PUT"])
+def update_task(index):
+
+    if index >= len(tasks):
+        return jsonify({"erro": "Tarefa não encontrada"}), 404
+
+    tasks[index] = request.json
+
+    return jsonify(tasks[index])
+
+
 @app.route("/tasks/<int:index>", methods=["DELETE"])
 def delete_task(index):
+
     if index >= len(tasks):
         return jsonify({"erro": "Tarefa não encontrada"}), 404
 
     tasks.pop(index)
+
     return jsonify({"mensagem": "Tarefa removida"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,53 @@
-from src.app import app
+from src.app import app, tasks
 
 
 def test_home():
+
     client = app.test_client()
 
     response = client.get("/")
+
+    assert response.status_code == 200
+
+
+def test_create_task():
+
+    tasks.clear()
+
+    client = app.test_client()
+
+    response = client.post(
+        "/tasks",
+        json={"titulo": "Estudar Engenharia de Software"}
+    )
+
+    assert response.status_code == 201
+
+
+def test_update_task():
+
+    tasks.clear()
+
+    tasks.append({"titulo": "Tarefa antiga"})
+
+    client = app.test_client()
+
+    response = client.put(
+        "/tasks/0",
+        json={"titulo": "Tarefa atualizada"}
+    )
+
+    assert response.status_code == 200
+
+
+def test_delete_task():
+
+    tasks.clear()
+
+    tasks.append({"titulo": "Excluir"})
+
+    client = app.test_client()
+
+    response = client.delete("/tasks/0")
 
     assert response.status_code == 200
